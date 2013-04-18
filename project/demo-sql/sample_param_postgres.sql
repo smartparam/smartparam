5078678f651ce21c41ca59dac910fc50f38b4098
Adam Dubiel <dubiel.adam@gmail.com>
2013-04-18 22:33:52 +0200

    * updated sql script for demo app

--- project/demo-sql/sample_param_postgres.sql
+++ project/demo-sql/sample_param_postgres.sql
@@ -1,12 +1,19 @@
 -- tested on POSTGRES
 
-insert into smartparam_function_impl values('java', nextval('seq_sp_function_impl'), 'org.smartparam.demo.param.LevelFunction', 'demoModelObject');
-insert into smartparam_function values(nextval('seq_sp_function'), true, 'level.sample', false, 'demoType', false, 1);
+insert into smartparam_parameter
+	(id, arrayseparator, cacheable, name, label, description, type, multivalue, nullable, archive, inputlevels, array_flag)
+	values(nextval('seq_sp_parameter'), ',', true, 'sample', 'Sample param', 'Sample param description', 'string', false, false, false, 0, false);
 
-insert into smartparam_parameter values(nextval('seq_sp_parameter'), false, false, ',', true, 'Sample param description', 0, 'Sample param', false, 'sample', false, 'string');
+insert into smartparam_level
+	(id, label, labelkey, parameter_id, type, orderno, array_flag, levelcreator)
+	values(nextval('seq_sp_level'), 'sample-level', 'smartparam.level.sample', 1, 'plugin', 0, false, 'level.sample');
 
-insert into smartparam_level values(nextval('seq_sp_level'), false, 'sample-level', 'smartparam.level.sample', null, 0, 'plugin', 1, 1, null);
-
-insert into smartparam_parameter_entry values(nextval('seq_sp_parameter_entry'), 'input1', null, null, null, null, null, null, null, 'output1', null, 1);
-insert into smartparam_parameter_entry values(nextval('seq_sp_parameter_entry'), 'input2', null, null, null, null, null, null, null, 'output2', null, 1);
-insert into smartparam_parameter_entry values(nextval('seq_sp_parameter_entry'), 'input3', null, null, null, null, null, null, null, 'output3', null, 1);
+insert into smartparam_parameter_entry
+	(id, function, level1, level2, level3, level4, level5, level6, level7, level8, value, parameter_id )
+	values(nextval('seq_sp_parameter_entry'), null, 'input1', null, null, null, null, null, null, null, 'output1', 1);
+insert into smartparam_parameter_entry
+	(id, function, level1, level2, level3, level4, level5, level6, level7, level8, value, parameter_id )
+	values(nextval('seq_sp_parameter_entry'), null, 'input2', null, null, null, null, null, null, null, 'output2', 1);
+	insert into smartparam_parameter_entry
+	(id, function, level1, level2, level3, level4, level5, level6, level7, level8, value, parameter_id )
+	values(nextval('seq_sp_parameter_entry'), null, 'input3', null, null, null, null, null, null, null, 'output3', 1);
